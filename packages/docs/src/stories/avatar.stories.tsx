f484783c9e056987c63e564c17022612677a4a5e
Jean Arruda <jean.arruda@thoughtworks.com>
2022-10-16 05:01:24 -0300

    adjust avatar component

--- packages/docs/src/stories/avatar.stories.tsx
+++ packages/docs/src/stories/avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/jefyelin.png',
     alt: 'Jean Arruda',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/react/src/components/avatar/index.tsx
+++ packages/react/src/components/avatar/index.tsx
@@ -14,3 +14,5 @@ export function Avatar(props: AvatarProps) {
     </Container>
   )
 }
+
+Avatar.displayName = 'Avatar'
